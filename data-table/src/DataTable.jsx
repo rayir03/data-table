61c74efb7d5b7f925db96126bf0324680ee6646a
rayir03 <rayir03@gmail.com>
2024-10-31 20:30:09 -0500

    Search and Pagination done

--- data-table/src/DataTable.jsx
+++ data-table/src/DataTable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef} from "react"
 
 
 const DataTable = () => {
@@ -7,6 +7,24 @@ const DataTable = () => {
     const [data, setData ] = useState([]);
     const [ editId, setEditId ] = useState(false);
     const outdideCkick = useRef(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [ searchTerm, setSearchTerm ] = useState("")
+
+    const itemsPerPage = 5;
+    const lastItem = currentPage * itemsPerPage;
+    const indexOfFirstitem = lastItem - itemsPerPage;
+    let filteredItems = data.filter((item) => (
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ))
+
+    
+
+    const filterData = filteredItems.slice(indexOfFirstitem, lastItem);
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [searchTerm])
+
     useEffect(()=> {
         if(!editId) return;
         let selectedItem = document.querySelectorAll(`[id="${editId}"]`)
@@ -44,6 +62,9 @@ const DataTable = () => {
     }
 
     const handleDelete = (id) => {
+        if(filterData.length === 1  && currentPage !== 1) {
+            setCurrentPage((prev) => prev - 1 );
+        }
         const updatedList = data.filter((item)=> item.id !== id )
         setData(updatedList);
     }
@@ -54,6 +75,15 @@ const DataTable = () => {
         }
         const updatedList = data.map((item) => item.id === id ? {...item, ...updatedData} : item);
         setData(updatedList);
+    }
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+
+    }
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber)
     }
 
   return (
@@ -70,7 +100,13 @@ const DataTable = () => {
       </div>
 
       <div className="search-table-container">
-        <input type="text" placeholder="Search by name" value={""} onChange={() => {}} className="search-input"/>
+        <input 
+            type="text" 
+            placeholder="Search by name" 
+            value={searchTerm} 
+            onChange={handleSearch} 
+            className="search-input"
+        />
 
         <table ref={outdideCkick}>
             <thead>
@@ -82,8 +118,7 @@ const DataTable = () => {
                 </tr>
             </thead>
             <tbody>
-               {data.length > 0 ? (
-                data.map((item) => (
+               {filterData.map((item) => (
                     <tr key={item.id}>
                         <td id={item.id} contentEditable={editId === item.id} onBlur={(e)=> handleEdit(item.id, {name: e.target.innerText})}>{item.name}</td>
                         <td id={item.id} contentEditable={editId === item.id} onBlur={(e)=> handleEdit(item.id, {gender: e.target.innerText})}>{item.gender}</td>
@@ -94,15 +129,20 @@ const DataTable = () => {
                         </td>
                     </tr>
                 ))
-               ) : (
-                <tr>
-                    <td colSpan="4">No data available</td>
-                </tr>
-               )}
+               }
                 
             </tbody>
         </table>
-        <div className="pagination"></div>
+        <div className="pagination">
+            {Array.from({length: Math.ceil(filteredItems.length / itemsPerPage) }, (_, index )=> (
+                <button 
+                    key={index + 1} 
+                    onClick={() => paginate(index+1)} 
+                    style={{backgroundColor: currentPage === index+1 && "lightgreen"}}>
+                    {index + 1}
+                </button>
+            ))}
+        </div>
       </div>
     </div>
   )
